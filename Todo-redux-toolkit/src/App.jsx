a3fdd03f0af112865f16584273a376141aefee81
Tafajjul Khan <tafajjulk607@gmail.com>
2025-10-02 01:21:05 +0530

    Todo app with Redux Toolkit and Tailwind CSS

---
+++ Todo-redux-toolkit/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import AddTodo from "./components/addTodo";
+import Todos from "./components/Todos.jsx"
+
+
+function App() {
+  
+  return (
+    <>
+      <h1>redux toolkit</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  );
+}
+
+export default App;
